db5b35d5fa9ed73875ba303a55d1a13600dd744d
Ester Carvalho <esterdesousa625@gmail.com>
2024-07-09 21:05:39 -0300

    primeiro commit

---
+++ js/servicos/servicos_produto.js
@@ -0,0 +1,31 @@
+const apiUrl = "http://localhost:3000/produtos";
+
+const listaProduto = () => {
+    return fetch(apiUrl)
+        .then((res) => res.json())
+        .catch((er) => console.log(er));
+};
+
+const criarProduto = (nome, preco, imagem) => {
+    return fetch(apiUrl, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ nome, preco, imagem }),
+    })
+        .then((res) => res.json())
+        .catch((er) => console.log(er));
+};
+
+const eliminarProduto = (id) => {
+    return fetch(`${apiUrl}/${id}`, {
+        method: "DELETE"
+    })
+        .then((res) => res.json())
+        .catch((err) => console.log(err));
+};
+
+export const servicosProdutos = {
+    listaProduto,
+    criarProduto,
+    eliminarProduto
+};
