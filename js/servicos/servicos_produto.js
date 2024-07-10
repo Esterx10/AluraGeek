835415b244cf1913f9fa2c999e1ae697708b8b31
Ester Carvalho <esterdesousa625@gmail.com>
2024-07-10 18:37:45 -0300

    modificando

--- js/servicos/servicos_produto.js
+++ js/servicos/servicos_produto.js
@@ -1,29 +1,38 @@
-// const apiUrl = "http://localhost:3000/produtos";
+const apiUrl = "http://localhost:3000/produtos";
 
-const apiUrl = "https://alura-geek-seven-wheat.vercel.app/";
+// const apiUrl = "https://alura-geek-seven-wheat.vercel.app/";
 
-const listaProduto = () => {
-    return fetch(apiUrl)
-        .then((res) => res.json())
-        .catch((er) => console.log(er));
+const listaProduto = async () => {
+    try {
+        const res = await fetch(apiUrl);
+        return await res.json();
+    } catch (er) {
+        return console.log(er);
+    }
 };
 
-const criarProduto = (nome, preco, imagem) => {
-    return fetch(apiUrl, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ nome, preco, imagem }),
-    })
-        .then((res) => res.json())
-        .catch((er) => console.log(er));
+const criarProduto = async (nome, preco, imagem) => {
+    try {
+        const res = await fetch(apiUrl, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ nome, preco, imagem }),
+        });
+        return await res.json();
+    } catch (er) {
+        return console.log(er);
+    }
 };
 
-const eliminarProduto = (id) => {
-    return fetch(`${apiUrl}/${id}`, {
-        method: "DELETE"
-    })
-        .then((res) => res.json())
-        .catch((err) => console.log(err));
+const eliminarProduto = async (id) => {
+    try {
+        const res = await fetch(`${apiUrl}/${id}`, {
+            method: "DELETE"
+        });
+        return await res.json();
+    } catch (err) {
+        return console.log(err);
+    }
 };
 
 export const servicosProdutos = {
