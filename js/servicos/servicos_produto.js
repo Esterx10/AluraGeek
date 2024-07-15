b6c2739a0a9f27b94f867cabb8050192c84f255c
Ester Carvalho <esterdesousa625@gmail.com>
2024-07-15 19:46:01 -0300

    commit-err

--- js/servicos/servicos_produto.js
+++ js/servicos/servicos_produto.js
@@ -1,35 +1,25 @@
-
 const listaProduto = async () => {
-    try {
-        const res = await fetch("https://json-test-rosy.vercel.app/produtos");
-        return await res.json();
-    } catch (err) {
-        return console.log(err);
-    }
-};
+        return fetch("https://json-test-rosy.vercel.app/produtos")
+            .then((res) => res.json())
+            .catch((err) => console.log(err));
+    };
 
 const criarProduto = async (nome, preco, imagem) => {
-    try {
-        const res = await fetch("https://json-test-rosy.vercel.app/produtos", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ nome, preco, imagem }),
-        });
-        return await res.json();
-    } catch (err) {
-        return console.log(err);
-    }
+    return fetch("https://json-test-rosy.vercel.app/produtos", {
+        method: "POST",
+        headers: {"Content-Type": "application/json",},
+        body: JSON.stringify({nome ,preco, imagem,}),
+    })
+        .then((res) => res.json())
+        .catch((err) => console.log(err));
 };
 
 const eliminarProduto = async (id) => {
-    try {
-        const res = await fetch(`${"https://json-test-rosy.vercel.app/produtos"}/${id}`, {
-            method: "DELETE"
-        });
-        return await res.json();
-    } catch (err) {
-        return console.log(err);
-    }
+    return fetch(`${"https://json-test-rosy.vercel.app/produtos"}/${id}`, {
+        method: "DELETE"
+    })
+        .then((res) => res.json())
+        .catch((err) => console.log(err));
 };
 
 export const servicosProdutos = {
