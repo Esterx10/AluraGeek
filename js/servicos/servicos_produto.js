1b2a12781e65e52a5bc411ce63dd09d09db18a51
Ester Carvalho <esterdesousa625@gmail.com>
2024-07-15 19:39:20 -0300

    commit-servicos

--- js/servicos/servicos_produto.js
+++ js/servicos/servicos_produto.js
@@ -1,32 +1,29 @@
-const apiUrl = "http://localhost:3000/produtos";
-
-
 
 const listaProduto = async () => {
     try {
-        const res = await fetch(apiUrl);
+        const res = await fetch("https://json-test-rosy.vercel.app/produtos");
         return await res.json();
-    } catch (er) {
-        return console.log(er);
+    } catch (err) {
+        return console.log(err);
     }
 };
 
 const criarProduto = async (nome, preco, imagem) => {
     try {
-        const res = await fetch(apiUrl, {
+        const res = await fetch("https://json-test-rosy.vercel.app/produtos", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ nome, preco, imagem }),
         });
         return await res.json();
-    } catch (er) {
-        return console.log(er);
+    } catch (err) {
+        return console.log(err);
     }
 };
 
 const eliminarProduto = async (id) => {
     try {
-        const res = await fetch(`${apiUrl}/${id}`, {
+        const res = await fetch(`${"https://json-test-rosy.vercel.app/produtos"}/${id}`, {
             method: "DELETE"
         });
         return await res.json();
